a4c6b4cf9c809e26398de6a0990396937cf792c8
lberrada <leonardbj@hotmail.fr>
2018-05-10 18:19:42 +0100

    dfw

--- mutils.py
+++ mutils.py
@@ -4,60 +4,26 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 #
-
-import re
-import inspect
-from torch import optim
+import numpy as np
+import torch
 
 
-def get_optimizer(s):
-    """
-    Parse optimizer parameters.
-    Input should be of the form:
-        - "sgd,lr=0.01"
-        - "adagrad,lr=0.1,lr_decay=0.05"
-    """
-    if "," in s:
-        method = s[:s.find(',')]
-        optim_params = {}
-        for x in s[s.find(',') + 1:].split(','):
-            split = x.split('=')
-            assert len(split) == 2
-            assert re.match("^[+-]?(\d+(\.\d*)?|\.\d+)$", split[1]) is not None
-            optim_params[split[0]] = float(split[1])
-    else:
-        method = s
-        optim_params = {}
+def adapt_grad_norm(model, max_grad_norm=None):
 
-    if method == 'adadelta':
-        optim_fn = optim.Adadelta
-    elif method == 'adagrad':
-        optim_fn = optim.Adagrad
-    elif method == 'adam':
-        optim_fn = optim.Adam
-    elif method == 'adamax':
-        optim_fn = optim.Adamax
-    elif method == 'asgd':
-        optim_fn = optim.ASGD
-    elif method == 'rmsprop':
-        optim_fn = optim.RMSprop
-    elif method == 'rprop':
-        optim_fn = optim.Rprop
-    elif method == 'sgd':
-        optim_fn = optim.SGD
-        assert 'lr' in optim_params
-    else:
-        raise Exception('Unknown optimization method: "%s"' % method)
+    if max_grad_norm is None:
+        return
 
-    # check that we give good parameters to the optimizer
-    expected_args = inspect.getargspec(optim_fn.__init__)[0]
-    assert expected_args[:2] == ['self', 'params']
-    if not all(k in expected_args[2:] for k in optim_params.keys()):
-        raise Exception('Unexpected parameters: expected "%s", got "%s"' % (
-            str(expected_args[2:]), str(optim_params.keys())))
-
-    return optim_fn, optim_params
-
+    shrink_factor = 1
+    total_norm = 0
+    for p in model.parameters():
+        if p.requires_grad:
+            total_norm += p.grad.data.norm().item() ** 2
+    total_norm = np.sqrt(total_norm)
+    if total_norm > max_grad_norm:
+        shrink_factor = max_grad_norm / total_norm
+    for p in model.parameters():
+        if p.requires_grad:
+            p.grad.data *= shrink_factor
 
 """
 Importing batcher and prepare for SentEval
--- train_nli.py
+++ train_nli.py
@@ -5,20 +5,26 @@
 # LICENSE file in the root directory of this source tree.
 #
 
-import os
 import sys
-import time
+sys.path.insert(1, '..')
+from optim import get_optimizer
+from utils import setup_xp, log_metrics, accuracy, regularization, update_metrics
+from cuda import set_cuda
+
+import sys
+import os
+import mlogger
 import argparse
-
 import numpy as np
-
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 
+from dfw import DFW
+from dfw.losses import MultiClassHingeLoss, set_smoothing_enabled
 from data import get_nli, get_batch, build_vocab
-from mutils import get_optimizer
+from mutils import adapt_grad_norm
 from models import NLINet
+from tqdm import tqdm
 
 
 GLOVE_PATH = "dataset/GloVe/glove.840B.300d.txt"
@@ -29,6 +35,10 @@ parser = argparse.ArgumentParser(description='NLI training')
 parser.add_argument("--nlipath", type=str, default='dataset/SNLI/', help="NLI data path (SNLI or MultiNLI)")
 parser.add_argument("--outputdir", type=str, default='savedir/', help="Output directory")
 parser.add_argument("--outputmodelname", type=str, default='model.pickle')
+parser.add_argument("--server", type=str, default='http://atlas.robots.ox.ac.uk')
+parser.add_argument("--port", type=int, default=9003)
+parser.add_argument('--no-tqdm', dest='tqdm', action='store_false', help="use of tqdm progress bars")
+parser.set_defaults(tqdm=True)
 
 
 # training
@@ -37,11 +47,15 @@ parser.add_argument("--batch_size", type=int, default=64)
 parser.add_argument("--dpout_model", type=float, default=0., help="encoder dropout")
 parser.add_argument("--dpout_fc", type=float, default=0., help="classifier dropout")
 parser.add_argument("--nonlinear_fc", type=float, default=0, help="use nonlinearity in fc")
-parser.add_argument("--optimizer", type=str, default="sgd,lr=0.1", help="adam or sgd,lr=0.1")
+parser.add_argument("--opt", type=str, default="sgd", help="choice of optimizer")
+parser.add_argument("--eta", type=float, default=0.1, help="initial learning rate")
+parser.add_argument("--momentum", type=float, default=0, help="momentum")
+parser.add_argument("--l2", type=float, default=0., help="l2-regularization")
 parser.add_argument("--lrshrink", type=float, default=5, help="shrink factor for sgd")
 parser.add_argument("--decay", type=float, default=0.99, help="lr decay")
 parser.add_argument("--minlr", type=float, default=1e-5, help="minimum lr")
 parser.add_argument("--max_norm", type=float, default=5., help="max norm (grad clipping)")
+parser.add_argument('--load-opt', default=None, help='data file with opt')
 
 # model
 parser.add_argument("--encoder_type", type=str, default='BLSTMEncoder', help="see list of encoders")
@@ -50,11 +64,13 @@ parser.add_argument("--n_enc_layers", type=int, default=1, help="encoder num lay
 parser.add_argument("--fc_dim", type=int, default=512, help="nhid of fc layers")
 parser.add_argument("--n_classes", type=int, default=3, help="entailment/neutral/contradiction")
 parser.add_argument("--pool_type", type=str, default='max', help="max or mean")
+parser.add_argument("--loss", type=str, default='svm', help="choice of loss function")
+parser.add_argument("--smooth-svm", dest="smooth_svm", action='store_true', help="smoothness of SVM")
+parser.set_defaults(smooth_svm=False)
 
 # gpu
-parser.add_argument("--gpu_id", type=int, default=3, help="GPU ID")
+parser.add_argument("--gpu_id", type=int, default=0, help="GPU ID")
 parser.add_argument("--seed", type=int, default=1234, help="seed")
-
 
 params, _ = parser.parse_known_args()
 
@@ -72,6 +88,8 @@ SEED
 np.random.seed(params.seed)
 torch.manual_seed(params.seed)
 torch.cuda.manual_seed(params.seed)
+
+torch.zeros(1).cuda()  # for quick initialization of process on device
 
 """
 DATA
@@ -117,41 +135,46 @@ encoder_types = ['BLSTMEncoder', 'BLSTMprojEncoder', 'BGRUlastEncoder',
                  'InnerAttentionNAACLEncoder', 'ConvNetEncoder', 'LSTMEncoder']
 assert params.encoder_type in encoder_types, "encoder_type must be in " + \
                                              str(encoder_types)
+
 nli_net = NLINet(config_nli_model)
 print(nli_net)
 
 # loss
-weight = torch.FloatTensor(params.n_classes).fill_(1)
-loss_fn = nn.CrossEntropyLoss(weight=weight)
-loss_fn.size_average = False
-
-# optimizer
-optim_fn, optim_params = get_optimizer(params.optimizer)
-optimizer = optim_fn(nli_net.parameters(), **optim_params)
+if params.loss == 'svm':
+    loss_fn = MultiClassHingeLoss()
+else:
+    loss_fn = nn.CrossEntropyLoss()
 
 # cuda by default
 nli_net.cuda()
 loss_fn.cuda()
+
+optimizer = get_optimizer(params, parameters=nli_net.parameters())
+
+params.visdom = True
+xp_name = '{}--{}--{}--eta-{}'.format(params.encoder_type, params.opt, params.loss, params.eta)
+if params.smooth_svm:
+    xp_name = xp_name + '--smooth'
+params.log = True
+params.outputdir = './saved/{}'.format(xp_name)
+params.xp_name = './xp_results/{}'.format(xp_name)
+
+if not os.path.exists(params.xp_name):
+    os.makedirs(params.xp_name)
+else:
+    raise ValueError('Experiment already exists at {}'.format(params.xp_name))
+xp = setup_xp(params, nli_net, optimizer)
 
 
 """
 TRAIN
 """
 val_acc_best = -1e10
-adam_stop = False
-stop_training = False
-lr = optim_params['lr'] if 'sgd' in params.optimizer else None
 
 
 def trainepoch(epoch):
     print('\nTRAINING : Epoch ' + str(epoch))
     nli_net.train()
-    all_costs = []
-    logs = []
-    words_count = 0
-
-    last_time = time.time()
-    correct = 0.
     # shuffle the data
     permutation = np.random.permutation(len(train['s1']))
 
@@ -159,99 +182,122 @@ def trainepoch(epoch):
     s2 = train['s2'][permutation]
     target = train['label'][permutation]
 
+    if epoch > 1 and params.opt == 'sgd':
+        optimizer.param_groups[0]['lr'] *= params.decay
+        optimizer.step_size = optimizer.param_groups[0]['lr']
 
-    optimizer.param_groups[0]['lr'] = optimizer.param_groups[0]['lr'] * params.decay if epoch>1\
-        and 'sgd' in params.optimizer else optimizer.param_groups[0]['lr']
-    print('Learning rate : {0}'.format(optimizer.param_groups[0]['lr']))
+    for metric in xp.train.metrics():
+        metric.reset()
 
-    for stidx in range(0, len(s1), params.batch_size):
+    for stidx in tqdm(range(0, len(s1), params.batch_size), disable=not params.tqdm,
+                      desc='Train Epoch', leave=False):
         # prepare batch
-        s1_batch, s1_len = get_batch(s1[stidx:stidx + params.batch_size],
-                                     word_vec)
-        s2_batch, s2_len = get_batch(s2[stidx:stidx + params.batch_size],
-                                     word_vec)
-        s1_batch, s2_batch = Variable(s1_batch.cuda()), Variable(s2_batch.cuda())
-        tgt_batch = Variable(torch.LongTensor(target[stidx:stidx + params.batch_size])).cuda()
-        k = s1_batch.size(1)  # actual batch size
+        s1_batch, s1_len = get_batch(s1[stidx:stidx + params.batch_size], word_vec)
+        s2_batch, s2_len = get_batch(s2[stidx:stidx + params.batch_size], word_vec)
+        s1_batch, s2_batch = s1_batch.cuda(), s2_batch.cuda()
+        tgt_batch = torch.LongTensor(target[stidx:stidx + params.batch_size]).cuda()
 
         # model forward
-        output = nli_net((s1_batch, s1_len), (s2_batch, s2_len))
-
-        pred = output.data.max(1)[1]
-        correct += pred.long().eq(tgt_batch.data.long()).cpu().sum()
-        assert len(pred) == len(s1[stidx:stidx + params.batch_size])
-
-        # loss
-        loss = loss_fn(output, tgt_batch)
-        all_costs.append(loss.data[0])
-        words_count += (s1_batch.nelement() + s2_batch.nelement()) / params.word_emb_dim
+        scores = nli_net((s1_batch, s1_len), (s2_batch, s2_len))
+        with set_smoothing_enabled(params.smooth_svm):
+            loss = loss_fn(scores, tgt_batch)
 
         # backward
         optimizer.zero_grad()
         loss.backward()
+        if not isinstance(optimizer, DFW):
+            adapt_grad_norm(nli_net, params.max_norm)
+        # necessary information for the step-size of some optimizers -> provide closure
+        optimizer.step(lambda: float(loss))
 
-        # gradient clipping (off by default)
-        shrink_factor = 1
-        total_norm = 0
+        # monitoring
+        batch_size = scores.size(0)
+        xp.train.acc.update(accuracy(scores, tgt_batch), weighting=batch_size)
+        xp.train.loss.update(loss_fn(scores, tgt_batch), weighting=batch_size)
+        xp.train.gamma.update(optimizer.gamma, weighting=batch_size)
 
-        for p in nli_net.parameters():
-            if p.requires_grad:
-                p.grad.data.div_(k)  # divide by the actual batch size
-                total_norm += p.grad.data.norm() ** 2
-        total_norm = np.sqrt(total_norm)
+    xp.train.eta.update(optimizer.eta)
+    if params.l2:
+        w_norm = torch.sqrt(sum(p.norm() ** 2 for p in nli_net.parameters()))
+        xp.train.reg.update(0.5 * params.l2 * xp.train.weight_norm.value ** 2)
+    else:
+        xp.train.reg.update(0)
+    xp.train.reg.update(0.5 * args.l2 * xp.train.weight_norm.value ** 2)
+    xp.train.obj.update(xp.train.reg.value + xp.train.loss.value)
+    xp.train.timer.update()
 
-        if total_norm > params.max_norm:
-            shrink_factor = params.max_norm / total_norm
-        current_lr = optimizer.param_groups[0]['lr'] # current lr (no external "lr", for adam)
-        optimizer.param_groups[0]['lr'] = current_lr * shrink_factor # just for update
-
-        # optimizer step
-        optimizer.step()
-        optimizer.param_groups[0]['lr'] = current_lr
-
-        if len(all_costs) == 100:
-            logs.append('{0} ; loss {1} ; sentence/s {2} ; words/s {3} ; accuracy train : {4}'.format(
-                            stidx, round(np.mean(all_costs), 2),
-                            int(len(all_costs) * params.batch_size / (time.time() - last_time)),
-                            int(words_count * 1.0 / (time.time() - last_time)),
-                            round(100.*correct/(stidx+k), 2)))
-            print(logs[-1])
-            last_time = time.time()
-            words_count = 0
-            all_costs = []
-    train_acc = round(100 * correct/len(s1), 2)
     print('results : epoch {0} ; mean accuracy train : {1}'
-          .format(epoch, train_acc))
-    return train_acc
+          .format(int(xp.epoch.value), xp.train.acc.value))
+    print('\nEpoch: [{0}] (Train) \t'
+          '({timer:.2f}s) \t'
+          'Obj {obj:.3f}\t'
+          'Loss {loss:.3f}\t'
+          'Acc {acc:.2f}%\t'
+          .format(int(xp.epoch.value),
+                  timer=xp.train.timer.value,
+                  acc=xp.train.acc.value,
+                  obj=xp.train.obj.value,
+                  loss=xp.train.loss.value))
+
+    for metric in xp.train.metrics():
+        metric.log(time=xp.epoch.value)
 
 
 def evaluate(epoch, eval_type='valid', final_eval=False):
     nli_net.eval()
-    correct = 0.
-    global val_acc_best, lr, stop_training, adam_stop
+    global val_acc_best, lr
 
     if eval_type == 'valid':
         print('\nVALIDATION : Epoch {0}'.format(epoch))
+        tag = 'val'
+    else:
+        tag = 'test'
+
+    if tag == 'val':
+        xp_group = xp.val
+    else:
+        xp_group = xp.test
+
+    for metric in xp_group.metrics():
+        metric.reset()
 
     s1 = valid['s1'] if eval_type == 'valid' else test['s1']
     s2 = valid['s2'] if eval_type == 'valid' else test['s2']
     target = valid['label'] if eval_type == 'valid' else test['label']
 
-    for i in range(0, len(s1), params.batch_size):
+    for i in tqdm(range(0, len(s1), params.batch_size), disable=not params.tqdm,
+                  desc='{} Epoch'.format(tag.title()), leave=False):
         # prepare batch
         s1_batch, s1_len = get_batch(s1[i:i + params.batch_size], word_vec)
         s2_batch, s2_len = get_batch(s2[i:i + params.batch_size], word_vec)
-        s1_batch, s2_batch = Variable(s1_batch.cuda()), Variable(s2_batch.cuda())
-        tgt_batch = Variable(torch.LongTensor(target[i:i + params.batch_size])).cuda()
+        s1_batch, s2_batch = s1_batch.cuda(), s2_batch.cuda()
+        tgt_batch = torch.LongTensor(target[i:i + params.batch_size]).cuda()
 
         # model forward
-        output = nli_net((s1_batch, s1_len), (s2_batch, s2_len))
+        scores = nli_net((s1_batch, s1_len), (s2_batch, s2_len))
+        batch_size = scores.size(0)
+        xp_group.acc.update(accuracy(scores, tgt_batch), weighting=batch_size)
 
-        pred = output.data.max(1)[1]
-        correct += pred.long().eq(tgt_batch.data.long()).cpu().sum()
+    xp_group.timer.update()
 
+    print('Epoch: [{0}] ({tag})\t'
+          '({timer:.2f}s) \t'
+          'Obj ----\t'
+          'Loss ----\t'
+          'Acc {acc:.2f}% \t'
+          .format(int(xp.epoch.value),
+                  tag=tag.title(),
+                  timer=xp_group.timer.value,
+                  acc=xp_group.acc.value))
+
+    if tag == 'val':
+        xp.max_val.update(xp.val.acc.value).log(time=xp.epoch.value)
+
+    for metric in xp_group.metrics():
+        metric.log(time=xp.epoch.value)
+
+    eval_acc = xp_group.acc.value
     # save model
-    eval_acc = round(100 * correct / len(s1), 2)
     if final_eval:
         print('finalgrep : accuracy {0} : {1}'.format(eval_type, eval_acc))
     else:
@@ -267,18 +313,11 @@ def evaluate(epoch, eval_type='valid', final_eval=False):
                        params.outputmodelname))
             val_acc_best = eval_acc
         else:
-            if 'sgd' in params.optimizer:
+            if 'sgd' in params.opt:
                 optimizer.param_groups[0]['lr'] = optimizer.param_groups[0]['lr'] / params.lrshrink
                 print('Shrinking lr by : {0}. New lr = {1}'
                       .format(params.lrshrink,
                               optimizer.param_groups[0]['lr']))
-                if optimizer.param_groups[0]['lr'] < params.minlr:
-                    stop_training = True
-            if 'adam' in params.optimizer:
-                # early stopping (at 2nd decrease in accuracy)
-                stop_training = adam_stop
-                adam_stop = True
-    return eval_acc
 
 
 """
@@ -286,19 +325,21 @@ Train model on Natural Language Inference task
 """
 epoch = 1
 
-while not stop_training and epoch <= params.n_epochs:
-    train_acc = trainepoch(epoch)
-    eval_acc = evaluate(epoch, 'valid')
-    epoch += 1
+with mlogger.stdout_to("{}/log.txt".format(params.xp_name)):
+    while epoch <= params.n_epochs:
+        xp.epoch.update(epoch)
+        trainepoch(epoch)
+        evaluate(epoch, 'valid')
+        epoch += 1
 
-# Run best model on test set.
-del nli_net
-nli_net = torch.load(os.path.join(params.outputdir, params.outputmodelname))
+    # Run best model on test set.
+    del nli_net
+    nli_net = torch.load(os.path.join(params.outputdir, params.outputmodelname))
 
-print('\nTEST : Epoch {0}'.format(epoch))
-evaluate(1e6, 'valid', True)
-evaluate(0, 'test', True)
+    print('\nTEST : Epoch {0}'.format(epoch))
+    evaluate(1e6, 'valid', True)
+    evaluate(0, 'test', True)
 
-# Save encoder instead of full model
-torch.save(nli_net.encoder,
-           os.path.join(params.outputdir, params.outputmodelname + '.encoder'))
+    # Save encoder instead of full model
+    torch.save(nli_net.encoder,
+               os.path.join(params.outputdir, params.outputmodelname + '.encoder'))
